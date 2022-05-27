6eb9f91f13ea0f9ad78ff07347d4ad1d274cc185
student <student@D01.local>
2022-05-27 12:16:51 +0700

    first commit

---
+++ G-Holiyay/G_HoliyayApp.swift
@@ -0,0 +1,17 @@
+//
+//  G_HoliyayApp.swift
+//  G-Holiyay
+//
+//  Created by student on 27/05/22.
+//
+
+import SwiftUI
+
+@main
+struct G_HoliyayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
